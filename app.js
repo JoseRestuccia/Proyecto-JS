b14452d1bfbf7f032136aead2f507e02a1922dcb
JoseRestuccia <jrestuccia15@gmail.com>
2022-07-31 20:40:24 -0300

    creando la estructura inicial de la calculadora de impuestos

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let i = 0;
+//* Calculadora de IVA *//
+const IVA = 1.21
 
-while (i < 5) {// muestra 0, luego 1, luego 2
-  alert( i );
- 
-  i++;}
+let precioProducto = true
+
+while (precioProducto) {
+    precioProducto = parseFloat(prompt("Ingrese el importe del producto que desea ingresar para calcular el IVA"))
+    console.log(precioProducto)
+    let respuesta = prompt("Desea ingresar mas precios?").toLocaleLowerCase()
+    
+    if (respuesta == "no"){
+        precioProducto = false
+    }
+    
+}
+
+function CalcularIVA(precioProducto = 0) {
+    return precioProducto * IVA 
+}
+
+
+let resultado = CalcularIVA
+console.log(resultado)
+
+
